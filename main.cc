f8de6d0d6b37c478a38108ac2752f63c2af0043e
liuyongfeng <531817981@qq.com>
2020-07-20 15:54:14 +0800

    两两交换链表中的节点：不正规

--- main.cc
+++ main.cc
@@ -12,42 +12,69 @@
 #include <stack>
 
 using namespace std;
-
-//todo 用stack
-std::vector<string> results;
-
-void generateParenthesis_(std::string current, int left, int right) // 当前string， 剩余左括号，剩余右括号
+struct ListNode
 {
-    if (left > right) //有右括号待填充
-    {
-        return;
-    }
-    if (left > 0) // 可以放left
-    {
-        generateParenthesis_(current + '(', left - 1, right);
-    }
-    if (right > 0 && right > left) // 可以放right
-    {
-        generateParenthesis_(current + ')', left, right - 1);
-    }
-    if (left == 0 && right == 0)
-    {
-        results.push_back(current);
-    }
-}
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 
-vector<string> generateParenthesis(int n)
+ListNode *swapPairs(ListNode *head)
 {
-    generateParenthesis_("", n, n);
-    return results;
+    if (!head)
+    {
+        return nullptr;
+    }
+    if (!head->next)
+    {
+        return head;
+    }
+    vector<ListNode *> v1;
+    vector<ListNode *> v2;
+    ListNode *p = head;
+    for (int i = 0; p != NULL; ++i)
+    {
+        if (i % 2)
+        {
+            v2.push_back(p);
+            p = p->next;
+        }
+        else
+        {
+            v1.push_back(p);
+            p = p->next;
+        }
+    }
+    for (int i = 0; i < v2.size(); ++i)
+    {
+        v2[i]->next = v1[i];
+        if (i + 1 < v2.size())
+            v1[i]->next = v2[i + 1];
+        else
+            v1[i]->next = NULL;
+    }
+    if (v1.size() > v2.size())
+    {
+        (*(v1.end() - 2))->next = *(v1.end() - 1);
+        *(v1.end() - 1) = NULL;
+    }
+    return v2[0];
 }
 
 int main()
 {
-    for (auto &&s : generateParenthesis(3))
+    ListNode *p = new ListNode(1);
+    p->next = new ListNode(2);
+    p->next->next = new ListNode(3);
+    // p->next->next->next = new ListNode(4);
+
+    p = swapPairs(p);
+    while (p)
     {
-        std::cout << s << std::endl;
+        std::cout << p->val << "\t";
+        p = p->next;
     }
+    std::cout << std::endl;
 
     return 0;
 }
