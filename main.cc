b2530f27f6b26c1c5a1e66068b46370921a18049
liuyongfeng <531817981@qq.com>
2020-07-21 11:10:16 +0800

    rm .h.in

--- main.cc
+++ main.cc
@@ -12,30 +12,60 @@
 #include <stack>
 
 using namespace std;
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
+// struct ListNode
+// {
+//     int val;
+//     ListNode *next;
+//     ListNode(int x) : val(x), next(NULL) {}
+// };
 
-int divide(int dividend, int divisor)
+void nextPermutation(vector<int> &nums)
 {
-    int op = ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) ? -1 : 1;
-    int sum = 0;
-    int times = 0;
-    dividend = dividend < 0 ? dividend : -dividend;
-    divisor = divisor < 0 ? divisor : -divisor;
-    while (sum > dividend)
+    if (nums.size() <= 1)
     {
-        if (sum < numeric_limits<int>::min() - divisor)
-        {
-            return numeric_limits<int>::max();
-        }
-        sum += divisor;
-        ++times;
+        return;
     }
-    return (times - 1) * op;
+    int j = nums.size() - 2, k = nums.size() - 1; // 和与那个数最近的大于它的数交换
+    while (nums[j] > nums[k] && j >= 0)
+    {
+        --j;
+        --k;
+    }
+
+    if (j == -1)
+    {
+        int smallest_index = 0;
+        int temp = 0;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (nums[i] < nums[smallest_index])
+            {
+                smallest_index = i;
+            }
+        }
+        temp = nums[smallest_index];
+        nums[smallest_index] = nums[j];
+        nums[j] = nums[smallest_index];
+    }
+    else
+    {
+        int swap_index = j + 1;
+        int smallest = nums[swap_index] - nums[j];
+        for (int i = j + 1; i < nums.size(); ++i)
+        {
+            int diff = nums[i] - nums[j];
+            if (diff > 0 && diff < smallest)
+            {
+                swap_index = i;
+            }
+        }
+        std::iter_swap(nums.begin() + j, nums.begin() + swap_index);
+    }
+    //todo 从j之后，都要排序
+    for (int i = j; i < nums.size(); ++i)
+    {
+        std::iter_swap(nums.begin() + i + 1, min_element(nums.begin() + i + 1, nums.end()));
+    }
 }
 
 int main()
@@ -44,8 +74,8 @@ int main()
     // p->next = new ListNode(2);
     // p->next->next = new ListNode(3);
     // p->next->next->next = new ListNode(4);
-
-    std::cout << divide(-2147483648, -1) << std::endl;
+    std::vector<int> vec = {1, 2};
+    nextPermutation(vec);
 
     // p = swapPairs(p);
     // while (p)
