d86b65c96865a862a0ae045c7363f7dd9cc81c50
liuyongfeng <531817981@qq.com>
2020-09-05 16:55:30 +0800

    vendingMachine problem

--- main.cc
+++ main.cc
@@ -1,12 +1,12 @@
 
-#pragma pack(push)
-#pragma pack(1)
+// #pragma pack(push)
+// #pragma pack(1)
 
 #include <string>
 #include <unordered_map>
 #include <iostream>
 #include <vector>
-#include <math.h>
+// #include <math.h>
 #include <numeric>
 #include <limits>
 #include <algorithm>
@@ -15,162 +15,231 @@
 #include <stack>
 #include <unordered_set>
 #include <set>
-#include <malloc.h>
-#include <semaphore.h>
-#include <pthread.h>
-#include <CivetServer.h>
-#include <thread>
-#include <string.h>
+// #include <malloc.h>
+// #include <semaphore.h>
+// #include <pthread.h>
+// #include <CivetServer.h>
+// #include <thread>
+// #include <string.h>
+#include <list>
 
-#include "test_function.h"
+// #include "test_function.h"
 
-sem_t sem;
-
-struct span
+class VendingMachine
 {
-    unsigned int smin : 16; // 占据16/8=2字节
-    char smax;              // 占据1字节，但是由于压缩，虽然没报错但是取不到地址，去掉的话真的会有11字节的占用
-    unsigned int area : 6;  // 占据（6/8）字节，最终按照对齐为1占据1字节
-    span *next;             // 占据8字节（64位机器）
+private:
+    const std::vector<int> m_costs;
+    std::vector<std::stack<int>> m_cargos;
+    uint m_total_costs = 0;
+    bool is_opened = false;
+
+public:
+    VendingMachine(const std::vector<int> &costs) : m_costs(costs), m_cargos(costs.size()) {}
+
+    void Open()
+    {
+        if (is_opened)
+        {
+            throw("?" + std::string(__FUNCTION__));
+        }
+        m_total_costs = 0;
+        is_opened = true;
+    }
+
+    int Close()
+    {
+        if (!is_opened)
+        {
+            throw("?" + std::string(__FUNCTION__));
+        }
+        is_opened = false;
+        int cost = m_total_costs;
+        m_total_costs = 0;
+        return cost;
+    }
+
+    void Return(int cargo_num, int good_num)
+    {
+        --cargo_num;
+        --good_num;
+        if (!is_opened)
+        {
+            throw("please open the vending first" + std::string(__FUNCTION__));
+        }
+        // 进货
+        m_cargos[cargo_num].push(good_num);
+        // 算钱
+        m_total_costs -= m_costs[good_num];
+    }
+
+    int Take(int cargo_num)
+    {
+        --cargo_num;
+        if (!is_opened)
+        {
+            throw("please open the vending first" + std::string(__FUNCTION__));
+        }
+        int goods;
+        if (m_cargos[cargo_num].empty())
+        {
+            // 出货
+            goods = cargo_num;
+        }
+        else
+        {
+            // 出货
+            goods = m_cargos[cargo_num].top();
+            m_cargos[cargo_num].pop();
+        }
+        m_total_costs += m_costs[goods];
+        return ++cargo_num;
+    }
+
+    ~VendingMachine() = default;
 };
 
-void InsertionSort(std::vector<int> &vec)
+enum class Hand
 {
-    //todo 有序区，无序区
-    //待排序就是i+1
-    size_t sz = vec.size();
-    for (size_t i = 1; i < vec.size(); i++)
+    LEFT,
+    RIGHT
+};
+
+class Customer
+{
+private:
+    int m_left = 0;
+    int m_right = 0;
+    std::list<int> m_goods;
+
+public:
+    Customer(/* args */) = default;
+    ~Customer() = default;
+
+    void Take(int goods_num, Hand h)
     {
-        size_t wait_for_sorted_index = i;
-        for (size_t j = wait_for_sorted_index - 1; j >= 0; j--)
+        if (h == Hand::LEFT)
         {
-            if (vec[j] < vec[wait_for_sorted_index])
+            if (m_left != 0)
             {
-                int swapped_value = vec[wait_for_sorted_index];
-                //todo push the nums on j's right hand
-                for (size_t k = wait_for_sorted_index; k > j + 1; k--)
-                {
-                    vec[k] = vec[k - 1];
-                }
-                vec[j + 1] = swapped_value;
-                //todo insert it at j
-                break;
+                throw("there is a goods in your current hand@" + std::string(__FUNCTION__));
             }
+            m_left = goods_num;
+        }
+        else
+        {
+            if (m_right != 0)
+            {
+                throw("there is a goods in your current hand@" + std::string(__FUNCTION__));
+            }
+            m_right = goods_num;
         }
     }
-}
 
-static int handler(struct mg_connection *conn, void *ignored)
-{
-    std::string msg = "Hello, you guys";
-    size_t len = msg.size();
-
-    mg_printf(conn, "HTTP/1.1 200 OK\r\n"
-                    "Content-Length: %lu\r\n"
-                    "Content-Type: text/plain\r\n"
-                    "Connection: close\r\n\r\n",
-              len);
-
-    mg_write(conn, msg.c_str(), msg.size());
-
-    return 200;
-}
-
-void static_thread_test()
-{
-    static bool called = false;
-
-    if (called)
+    int GetLeftHand(Hand h)
     {
-        std::cout << "it has been called" << std::endl;
-        return;
-    }
-    else
-    {
-        std::cout << "calling" << std::endl;
-        called = true;
+        int goods;
+        if (h == Hand::LEFT)
+        {
+            goods = m_left;
+            m_left = 0;
+        }
+        else
+        {
+            goods = m_right;
+            m_right = 0;
+        }
+        return goods;
     }
 
-    return;
-}
-
-const int generation_times = 10;
-thread_local std::vector<int> num_array(generation_times);
-void fill_vec(int start)
-{
-    std::iota(num_array.begin(), num_array.end(), start);
-    for (auto &i : num_array)
+    void Keep(Hand h)
     {
-        std::cout << i << '\t';
+        if (h == Hand::LEFT)
+        {
+            if (m_left == 0)
+            {
+                throw("no goods in your left hand@" + std::string(__FUNCTION__));
+            }
+            m_goods.push_back(m_left);
+            m_left = 0;
+        }
+        else
+        {
+            if (m_right == 0)
+            {
+                throw("no goods in your right hand@" + std::string(__FUNCTION__));
+            }
+            m_goods.push_back(m_right);
+            m_right = 0;
+        }
     }
-    std::cout << std::endl;
-}
+};
 
 int main(int argc, char **argv)
 {
+    int cargo_size, customer_size;
+    std::cin >> cargo_size >> customer_size;
+    std::vector<int> total_costs(customer_size);
+    std::vector<int> cargo_costs(cargo_size);
+    for (size_t i = 0; i < cargo_size; ++i)
     {
-        fill_vec(1);
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::thread th(fill_vec, 1000);
-        th.join();
-        return 0;
-    }
-    {
-        static_thread_test();
-        std::thread th(static_thread_test);
-        std::this_thread::sleep_for(std::chrono::seconds(3));
-        std::thread th2(static_thread_test);
-
-        th.join();
-        th2.join();
-        return 0;
+        scanf("%d", &cargo_costs[i]);
     }
 
+    VendingMachine vm(cargo_costs);
+
+    for (size_t i = 0; i < customer_size; ++i)
     {
-        struct mg_context *ctx;
+        Customer c;
+        vm.Open();
+        int op_size;
+        scanf("%d", &op_size);
+        for (size_t j = 0; j < op_size; ++j)
+        {
+            std::string op, hand, cargo_num;
+            std::cin >> hand >> op;
+            if (op != "keep")
+            {
+                std::cin >> cargo_num;
+            }
 
-        mg_init_library(0);
-
-        ctx = mg_start(NULL, 0, NULL); //我估计从这一句开始服务器就开始执行了。
-
-        mg_set_request_handler(ctx, "/lolipop", handler, (void *)"Hello. I am your lolipop!");
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(30000)); //sleep多久服务器就开放多久，应该有manager线程和worker线程在做事
-
-        mg_stop(ctx);
-
-        mg_exit_library();
+            if (hand == "left")
+            {
+                if (op == "take")
+                {
+                    c.Take(vm.Take(std::stoi(cargo_num)), Hand::LEFT);
+                }
+                else if (op == "return")
+                {
+                    vm.Return(std::stoi(cargo_num), c.GetLeftHand(Hand::LEFT));
+                }
+                else if (op == "keep")
+                {
+                    c.Keep(Hand::LEFT);
+                }
+            }
+            else
+            {
+                if (op == "take")
+                {
+                    c.Take(vm.Take(std::stoi(cargo_num)), Hand::RIGHT);
+                }
+                else if (op == "return")
+                {
+                    vm.Return(std::stoi(cargo_num), c.GetLeftHand(Hand::RIGHT));
+                }
+                else if (op == "keep")
+                {
+                    c.Keep(Hand::RIGHT);
+                }
+            }
+        }
+        total_costs[i] = vm.Close();
     }
 
+    for (int i : total_costs)
     {
-        std::vector<int> vec = {11, 123, 14, 5, 15, 6, 7, 8, 1123, 689, 9, 1};
-        InsertionSort(vec);
+        std::cout << i << std::endl;
     }
-
-    {
-        A *a = new C();
-        auto *b = static_cast<B *>(a);
-        b->Oh();
-        delete a;
-    }
-
-    std::cout << "size\t" << sizeof(span) << std::endl;
-    span s;
-    std::cout << "&\t" << &s << std::endl;
-    // std::cout << "&smin\t" << &(a.smin) << std::endl;
-    std::cout << "&smax\t" << &(s.smax) << std::endl;
-    // std::cout << "&area\t" << &(a.area) << std::endl;
-    std::cout << "&next\t" << &(s.next) << std::endl;
-
-    int *a = new int[10];
-    auto a_size = malloc_usable_size(a);
-
-    int *b = new int(6226);
-    auto b_size = malloc_usable_size(b);
-
-    auto int_size = sizeof(int);
 
     return 0;
 }
-
-#pragma pack(pop)
