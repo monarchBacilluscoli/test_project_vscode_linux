0462f6023b105b2ce7acf5d80c9089741c56880f
liuyongfeng <531817981@qq.com>
2020-09-08 18:42:37 +0800

    passwordCheck

--- main.cc
+++ main.cc
@@ -28,99 +28,78 @@
 
 // #include "test_function.h"
 
-const std::vector<std::pair<int, int>> g_directions = {
-    {0, 1},
-    {1, 0},
-    {0, -1},
-    {-1, 0}};
-
-std::mutex mtx;
-
-int func()
+int PasswordCheck(const std::string &pwd)
 {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << __FUNCTION__ << std::endl;
-    return 444;
-}
+    //todo 长度 8-120
 
-void print_v(int &a)
-{
-    std::cout << a << std::endl;
+    size_t sz = pwd.size();
+    if (sz > 120 || sz < 8)
+    {
+        return 1;
+    }
+
+    bool has_int = false, has_char = false, has_upper = false, has_lower = false;
+
+    //todo 类型：数字，符号，大写，小写
+    for (size_t i = 0; i < sz; ++i)
+    {
+        char c = pwd[i];
+        if (c <= 57 && c >= 48)
+        {
+            if (!has_int)
+            {
+                has_int = true;
+            }
+        }
+        else if (c <= 90 && c >= 65)
+        {
+            if (!has_upper)
+            {
+                has_upper = true;
+            }
+        }
+        else if (c <= 122 && c >= 97)
+        {
+            if (!has_lower)
+            {
+                has_lower = true;
+            }
+        }
+        else
+        {
+            if (!has_char)
+            {
+                has_char = true;
+            }
+        }
+    }
+    if (has_char && has_int && has_lower && has_upper)
+    {
+        return 0;
+    }
+    else
+    {
+        return 2;
+    }
 }
 
 int main(int argc, char **argv)
 {
+    std::vector<std::string> ss;
+    std::string s;
+
+    while (std::cin >> s)
     {
-        const int a = 100;
-        const int *b = &a;
-        // print_v()
-        int *c = const_cast<int *>(b);
-        // print_v(b);
-        // print_v();
-        *c = 101;
-        return 0;
-    }
-
-    {
-        std::future<int> f = std::async(std::launch::async, func);
-        f.wait();
-        std::cout << f.get() << std::endl;
-        f.wait();
-        std::cout << f.get() << std::endl;
-    }
-
-    {
-        mtx.lock();
-
-        mtx.lock();
-
-        std::cout << "it can be locked twice" << std::endl;
-
-        mtx.unlock();
-    }
-    int data_size;
-    std::cin >> data_size;
-    for (size_t i = 0; i < data_size; ++i)
-    {
-        std::unordered_map<int, std::unordered_map<int, int>> min_distance_map;
-        std::pair<int, int> current_pos = {0, 0};
-        min_distance_map[0][0] = 0;
-        int op_size;
-        std::cin >> op_size;
-        for (size_t j = 0; j < op_size; ++j)
+        ss.emplace_back(s);
+        if (std::cin.peek() == '\n')
         {
-            int dir;
-            int moveable;
-            //todo 解析操作
-            std::cin >> dir >> moveable;
-            if (moveable == 1)
-            {
-                int current_min_dis = min_distance_map[current_pos.first][current_pos.second] + 1;
-                current_pos.first += g_directions[dir].first;
-                current_pos.second += g_directions[dir].second;
-                //todo 检查并更新距离
-                for (const auto &i : g_directions)
-                {
-                    if (min_distance_map.find(current_pos.first + i.first) != min_distance_map.end() &&
-                        min_distance_map[current_pos.first + i.first].find(current_pos.second + i.second) != min_distance_map[current_pos.first + i.first].end())
-                    {
-                        if (min_distance_map[current_pos.first + i.first][current_pos.second + i.second] + 1 < current_min_dis)
-                        {
-                            current_min_dis = min_distance_map[current_pos.first + i.first][current_pos.second + i.second] + 1;
-                        }
-                    }
-                }
-                min_distance_map[current_pos.first][current_pos.second] = current_min_dis;
-                if (j == op_size - 1)
-                {
-                    std::cout << current_min_dis << std::endl;
-                }
-            }
-            else
-            {
-                ;
-            }
+            break;
         }
+    }
+
+    for (const auto &s : ss)
+    {
+        std::cout << PasswordCheck(s) << std::endl;
     }
 
     return 0;
