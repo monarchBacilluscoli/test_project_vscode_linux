506e39896d50a5c5ba95ecab5c8dda386f8228c4
liuyongfeng <531817981@qq.com>
2020-09-09 15:16:11 +0800

    program runs from the true catch

--- main.cc
+++ main.cc
@@ -2,77 +2,47 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <future>
 
-// from (i,j) check word[w];
-bool extend(std::vector<std::vector<char>> &mtx, const std::string &word, int i, int j, int w)
+void Thrower()
 {
-    int x_sz = mtx.front().size(), y_sz = mtx.size();
-    if (i < 0 || j < 0 || i >= y_sz || j >= x_sz)
+    try
     {
-        return false;
+        std::cout << "before calling@" + std::string(__FUNCTION__) << std::endl;
+        throw(std::range_error("an error!"));
+        std::cout << "after calling@" + std::string(__FUNCTION__) << std::endl;
     }
-    else
+    catch (const std::bad_cast &e)
     {
-        // if all the checks from here false, set the value to 0;
-        if (word[w] == mtx[i][j])
-        {
-            if (w == word.size() - 1)
-            {
-                return true;
-            }
-            else
-            {
-                mtx[i][j] = 0;
-                if (extend(mtx, word, i - 1, j, w + 1) ||
-                    extend(mtx, word, i + 1, j, w + 1) ||
-                    extend(mtx, word, i, j - 1, w + 1) ||
-                    extend(mtx, word, i, j + 1, w + 1))
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
-        else
-        {
-            return false;
-        }
+        std::cerr << e.what() << '\n';
     }
 }
-std::vector<std::vector<char>> g_matrix = {
-    {'A', 'B', 'C', 'E'},
-    {'S', 'F', 'C', 'S'},
-    {'A', 'D', 'E', 'E'}};
 
-bool WordCheck(const std::vector<std::vector<char>> &board, const std::string &word)
+void Caller()
 {
-    int x_sz = board.front().size(), y_sz = board.size();
-    for (size_t i = 0; i < y_sz; ++i)
+    try
     {
-        for (size_t j = 0; j < x_sz; ++j)
-        {
-            std::vector<std::vector<char>> bd = board;
-            if (extend(bd, word, i, j, 0))
-            {
-                return true;
-            }
-        }
+        std::cout << "before calling@" + std::string(__FUNCTION__) << std::endl;
+        Thrower();
+        std::cout << "after calling@" + std::string(__FUNCTION__) << std::endl;
     }
-
-    return false;
+    catch (const std::runtime_error &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << "after catch@" + std::string(__FUNCTION__) << std::endl;
 }
 
+//todo test1: multithread exception - can main thread catch the exception from child thread
+//todo test2: after catch - which statement in which layer will be executed
 int main(int argc, char **argv)
 {
-    std::cout << 79 / 20 << std::endl;
-
-    std::string s;
-    std::cin >> s;
-
-    std::cout << (WordCheck(g_matrix, s) ? "true" : "false") << std::endl;
-
+    Caller(); /* The output:
+    before calling@Caller
+    before calling@Thrower
+    an error!
+    after catch@Caller
+    */
+              //也就是说控制流会从真正catch了这个异常的那个catch之后继续运行，而不是从throw的位置继续运行，也不是从catch的那一层的throw出来的位置继续运行。
     return 0;
 }
