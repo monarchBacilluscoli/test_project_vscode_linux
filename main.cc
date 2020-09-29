70b3ed0746ad74cc581f7c0a701e4e76e6c2e336
liuyongfeng <531817981@qq.com>
2020-09-29 16:19:18 +0800

    *bitset cover bits

--- main.cc
+++ main.cc
@@ -4,21 +4,30 @@
 #include <map>
 #include <unordered_set>
 #include <cstring>
+#include <bitset>
+
+uint32_t CoverBits(uint32_t src, uint32_t tar, uint16_t start, uint16_t size)
+{
+    std::bitset<32> sb(src);
+    std::cout << "src:\t" << sb << std::endl;
+    std::bitset<32> tb(tar);
+    std::cout << "tar:\t" << tb << std::endl;
+
+    for (size_t i = 0; i < size; ++i)
+    {
+        size_t index = start + i;
+        tb[index] = sb[index];
+    }
+    std::cout << "Cov:\t" << tb << std::endl;
+    return tb.to_ulong();
+}
 
 int main(int argc, char **argv)
 {
-    int start, end;
-    int test = std::numeric_limits<int>::max();
-    std::cin >> start >> end;
-    test = test << (31 - end);
-    test = test >> (31 - end + start);
-    test = test << (start);
-    std::cout << test << std::endl;
-
-    std::map<int, int> mmm = {{1, 2}, {2, 3}};
-    mmm = {{3, 4}};
-
-    
+    uint32_t a, b;
+    uint start, size;
+    std::cin >> a >> b >> start >> size;
+    CoverBits(a, b, start, size);
 
     return 0;
 }
